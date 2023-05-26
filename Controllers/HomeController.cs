f6e274011f697dead2d3d98386407b87102c5e18
HoangAnh <johnyanhn01@gmail.com>
2023-05-26 21:34:03 +0700

    first commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,32 +1,26 @@
-﻿using System.Diagnostics;
+﻿using Exam.Models;
 using Microsoft.AspNetCore.Mvc;
-using Exam.Models;
+using System.Diagnostics;
 
-namespace Exam.Controllers;
-
-public class HomeController : Controller
+namespace Exam.Controllers
 {
-    private readonly ILogger<HomeController> _logger;
-
-    public HomeController(ILogger<HomeController> logger)
+    public class HomeController : Controller
     {
-        _logger = logger;
-    }
 
-    public IActionResult Index()
-    {
-        return View();
-    }
+        public IActionResult Index()
+        {
+            return View();
+        }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
+        public IActionResult Add()
+        {
+            return View();
+        }
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        [HttpPost]
+        public IActionResult Add(FormCollection form)
+        {
+            return View();
+        }
     }
 }
-
